e6408cc1f3873207c167178cb0687f93ac41b0c3
Luis Faria <lfariabr@gmail.com>
2026-07-18 17:08:54 +1000

    feat(absa-xai): add indicative ATAE token evidence (Relates #85)

---
+++ tests/absa/test_attention.py
@@ -0,0 +1,7 @@
+from src.absa.interpretability.attention import EVIDENCE_CAVEAT, align_attention
+
+
+def test_attention_alignment_uses_visible_tokens_only():
+    evidence = align_attention("Great food!", [0.2, 0.7, 0.1, 0.0])
+    assert evidence == [{"token": "great", "weight": 0.2}, {"token": "food", "weight": 0.7}, {"token": "!", "weight": 0.1}]
+    assert "not causal" in EVIDENCE_CAVEAT
